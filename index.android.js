3c5492b962cc02e4c3c7f3016c09f0bd5e0eb561
mradzwilla <mradzwilla@gmail.com>
2017-05-07 18:25:41 -0400

    Initial commit

---
+++ index.android.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import SmarterWeather from './weather_project';
+
+import DropIt from "./DropIt.js"
+AppRegistry.registerComponent('DropIt_Mobile', () => DropIt);

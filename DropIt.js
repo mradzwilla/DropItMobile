af00712db7e5cba637b46de13d2923f7ce06b23d
mradzwilla <mradzwilla@gmail.com>
2017-05-07 22:45:03 -0400

    Added map

--- DropIt.js
+++ DropIt.js
@@ -1,34 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  AsyncStorage,
   View
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class DropIt extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+constructor(props){
+	super(props);
+	this.state = {
+	  region: { 
+	  	    latitude: 40.642625,
+          	longitude: -74.014391,
+          	latitudeDelta: 0.092,
+	    	longitudeDelta: 0.0621,
+  	}
+}
+this.onRegionChange = this.onRegionChange.bind(this)
+this.setState = this.setState.bind(this)
+}
+
+onRegionChange(region) {
+  this.setState({ region });
+}
+
+componentWillMount() {
+	this._getCoordinates()
+}
+
+
+_getCoordinates(){
+    navigator.geolocation.getCurrentPosition(
+      (initialPosition) => {
+      	console.log(initialPosition.coords.latitude)
+      	return initialPosition
+      },
+      (error) => {alert(error.message)},
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     );
-  }
+}
+
+render() {
+  return (
+    <MapView
+      region={this.state.region}
+      onRegionChange={this.onRegionChange}
+      style = {{flex:1}}
+    />
+  );
+}
 }
 
 const styles = StyleSheet.create({
@@ -49,5 +72,3 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
-
-export default DropIt
